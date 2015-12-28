42bff3010b3d0ac11585e7fa37417041ce92cd19
Hector Gabucio <hectordorito5@gmail.com>
2015-12-28 22:57:32 +0100

    panel administracion categorias

--- application/controllers/tienda.php
+++ application/controllers/tienda.php
@@ -1,18 +1,24 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Categoria extends CI_Controller {
+class Tienda extends CI_Controller {
 
   function __construc(){
     parent::__construc();
     $this->load->database();
   }
 
-  public function index($idtienda)
+
+  public function admin($id) {
+    $data['titulo']="Panel de control de la tienda " . $id;
+    $data['id'] = $id;
+    $this->load->view('tienda-bo/index', $data);
+  }
+/*
+  public function index()
 	{
 
     $this->load->model("Categoria_m",'', TRUE);
     $data['titulo']="Listado de categorías";
-    $data['idtienda']=$idtienda;
     //$data['cuantos']=$this->Agenda_m->count_all();
     $data['lista']=$this->Categoria_m->view();
 
@@ -26,18 +32,5 @@ class Categoria extends CI_Controller {
     $nombre = $_POST['nuevacat'];
     $this->Categoria_m->add($nombre);
   }
-
-  public function borrar($idcat)
-  {
-    $this->load->model("Categoria_m",'', TRUE);
-    $this->Categoria_m->delete($idcat);
-  }
-
-  public function editar($idcat)
-  {
-    $this->load->model("Categoria_m",'', TRUE);
-    $nombre = $_POST['nuevonombre'];
-    $this->Categoria_m->update($idcat,$nombre);
-  }
-
+*/
 }
